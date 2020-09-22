0a8c2f6e668dfe478d07a44d6f6070390cb4905e
Santhosh J <santhosh@almabase.com>
2020-09-22 20:07:50 +0530

    daded static root

--- overwatch_heroes/settings.py
+++ overwatch_heroes/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-   ]
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
