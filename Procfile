0c722279cd0e9d7db9e5ce6914a6ee41c22bc421
Santhosh J <santhosh@almabase.com>
2020-09-22 21:07:18 +0530

    some changes

--- Procfile
+++ Procfile
@@ -1,4 +1,1 @@
-release:python manage.py makemigrations --no-input
-release:python manage.py migrate --no-input
-
-web:gunicorn overwatch_heroes.wsgi
+web: gunicorn overwatch_heroes.wsgi
--- overwatch_heroes/settings.py
+++ overwatch_heroes/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import django_filters
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,5 +129,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
-
-django_heroku.settings(locals())
