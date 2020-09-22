59db1e118538a5e559d73a0ff877871aa9e67acb
Santhosh J <santhosh@almabase.com>
2020-09-22 20:44:25 +0530

    added proper api app

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-web: gunicorn overwatch_heroes
+release: python manage.py makemigrations --no-input
+release: python manage.py migrate --no-input
+
+web: gunicorn heroes.wsgi
--- overwatch_heroes/settings.py
+++ overwatch_heroes/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import django_filters
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
+
+django_heroku.settings(locals())
